4b20ab93f6e3c1c9c2465ee9bfcf53a45b68c43a
sifaazhari <sitifatimah.azhri@gmail.com>
2023-05-05 20:49:08 +0700

    latihan5

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -5,32 +5,80 @@ class mahasiswa extends CI_Controller
     {
         parent::__construct();
         $this->load->model('m_mahasiswa');
+        $this->load->library(array('template', 'pagination', 'form_validation'));
     }
     function index()
     {
         $data['mahasiswa'] = $this->m_mahasiswa->tampilData()->result();
-        $this->load->view('mahasiswa', $data);
+        // $this->load->view('mahasiswa', $data);
+        $this->template->display('mahasiswa/index', $data);
     }
 
-    function hapus($nim)
+    // function hapus($nim)
+    // {
+    //     $this->m_mahasiswa->hapus($nim);
+
+    //     $data['mahasiswa'] = $this->m_mahasiswa->tampilData()->result();
+    //     $this->load->view('mahasiswa', $data);
+    // }
+    function hapus()
     {
+        $nim = $this->input->post('kode');
         $this->m_mahasiswa->hapus($nim);
 
         $data['mahasiswa'] = $this->m_mahasiswa->tampilData()->result();
         $this->load->view('mahasiswa', $data);
     }
-    function savingdata()  
-    {  
+    function savingdata()
+    {
         //this array is used to get fetch data from the view page.  
-        $data = array(  
-                        'nim'     => $this->input->post('nim'),  
-                        'nama'  => $this->input->post('nama'),  
-                        'jurusan'   => $this->input->post('jurusan'),  
-                        'alamat' => $this->input->post('alamat')  
-                        );  
+        $data = array(
+            'nim'     => $this->input->post('nim'),
+            'nama'  => $this->input->post('nama'),
+            'jurusan'   => $this->input->post('jurusan'),
+            'alamat' => $this->input->post('alamat')
+        );
         //insert data into database table.  
-        $this->db->insert('mahasiswa',$data);  
-  
-        redirect("mahasiswa/index");  
-    }  
+        $this->m_mahasiswa->simpanData($data);
+        redirect("mahasiswa/index");
+    }
+    function tambah()
+    {
+        $this->template->display('mahasiswa/tambah');
+    }
+    // function edit($nim)
+    // {
+    //     $where = array('nim' => $nim);
+    //     $data['mahasiswa'] = $this->m_mahasiswa->edit_data($where, 'mahasiswa')->result();
+    //     $this->load->view('edit_mahasiswa', $data);
+    // }
+    function edit($nim)
+    {
+        $where = array('nim' => $nim);
+        $data['mhs'] = $this->m_mahasiswa->edit_data($where, 'mahasiswa')->row_array();
+        // var_dump($data);
+        // exit();
+        $this->template->display('mahasiswa/edit', $data);
+    }
+    
+    function update()
+    {
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $jurusan = $this->input->post('jurusan');
+        $alamat = $this->input->post('alamat');
+
+        $data = array(
+            'nama' => $nama,
+            'jurusan' => $jurusan,
+            'alamat' => $alamat
+        );
+
+        $where = array(
+            'nim' => $nim
+        );
+
+        $this->m_mahasiswa->update_data($where, $data, 'mahasiswa');
+        redirect('mahasiswa/index');
+    }
 }
